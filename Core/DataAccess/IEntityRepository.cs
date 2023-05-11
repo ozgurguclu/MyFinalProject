cdaaa316e715a8b22b3ae6d8ac401cccd0c24c1a
ozgurguclu <64900481+ozgurguclu@users.noreply.github.com>
2023-05-11 17:43:24 +0300

    11.05.2023

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,15 +1,15 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     // Generic constraint
     // class: referans tip
     // IEntity olabilir ya da IEntity implemente eden bir nesne olabilir
-    // New'lenebilir olmalı, soyut olmamalı, parametresiz constructer olmalı
+    // New'lenebilir olmalı
     public interface IEntityRepository<T> where T:class, IEntity, new()
     {
         List<T> GetAll(Expression<Func<T, bool>> filter = null);
