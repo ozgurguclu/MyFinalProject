d89cbc4af3998d88bf97355245738b58234d3d75
ozgurguclu <64900481+ozgurguclu@users.noreply.github.com>
2023-05-19 16:00:01 +0300

    19.05.2023

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,8 +1,10 @@
 ﻿using Business.Abstract;
+using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -10,6 +12,7 @@ using Entities.DTOs;
 using FluentValidation;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Business.Concrete
@@ -17,16 +20,23 @@ namespace Business.Concrete
     public class ProductManager : IProductService
     {
         IProductDal _productDal;
+        ICategoryService _categoryService;
 
-        public ProductManager(IProductDal productDal)
+        public ProductManager(IProductDal productDal, ICategoryService categoryService)
         {
             _productDal = productDal;
+            _categoryService = categoryService;
         }
 
-        [ValidationAspect(typeof(ProductValidator))]
+        //[ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            // business codes
+            IResult result = BusinessRules.Run(CheckIfProductNameExists(product.ProductName), 
+                CheckIfProductCountOfCategory(product.CategoryId),
+                CheckIfCategoryLimitExceeded(15));
+
+            if (result != null)
+                return result;
 
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
@@ -62,8 +72,38 @@ namespace Business.Concrete
         public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
             //if (DateTime.Now.Hour == 16)
-                //return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime);
+            //return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime);
             return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
+        }
+
+        [ValidationAspect(typeof(ProductValidator))]
+        public IResult Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IResult CheckIfProductCountOfCategory(int categoryId)
+        {
+            int productCountOfCategory = _productDal.GetAll(p => p.CategoryId == categoryId).Count;
+            if (productCountOfCategory >= 15)
+                return new ErrorResult(Messages.ProductCountOfCategoryError);
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfProductNameExists(string productName)
+        {
+            var result = _productDal.GetAll(p => p.ProductName == productName).Any();
+            if (result)
+                return new ErrorResult(Messages.ProductNameAlreadyExists);
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfCategoryLimitExceeded(int number)
+        {
+            var result = _categoryService.GetAll();
+            if(result.Data.Count >= number)
+                return new ErrorResult(Messages.CategoryLimitExceeded);
+            return new SuccessResult();
         }
     }
 }
